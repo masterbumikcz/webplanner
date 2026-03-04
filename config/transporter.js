09cf705f08ec798977c58b5d5874eea3302e225c
Lukášek <masterboomcz@seznam.cz>
2026-03-04 20:10:17 +0100

    Předělání transporteru na brevo

--- config/transporter.js
+++ config/transporter.js
@@ -1,3 +1,4 @@
+/*
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
@@ -11,6 +12,45 @@ const transporter = nodemailer.createTransport({
 });
 
 console.log(
-  `Transporter configured with Gmail user: ${process.env.GMAIL_USER}`,
+  `Transporter configured with Gmail user: ${process.env.GMAIL_USER}`
+);
+export default transporter;
+*/
+
+const transporter = {
+  sendMail: async (mailOptions) => {
+    const payload = {
+      sender: {
+        name: "WebPlanner Application",
+        email: process.env.GMAIL_USER,
+      },
+      to: [{ email: mailOptions.to }],
+      subject: mailOptions.subject,
+      textContent: mailOptions.text,
+    };
+
+    const response = await fetch("https://api.brevo.com/v3/smtp/email", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "api-key": process.env.BREVO_API_KEY,
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      console.error("Error response from Brevo API:", errorData);
+      throw new Error(`Failed to send email: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+  },
+};
+
+console.log(
+  "Transporter configured with Brevo API key: " + process.env.BREVO_API_KEY,
 );
 export default transporter;
